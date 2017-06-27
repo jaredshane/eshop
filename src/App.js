2a15fcb7344c5db3b8afa7ffd82fc7ae9e7b9821
Jared Fuller <jaredshane@gmail.com>
2017-06-27 13:38:24 -0500

    testing some ways to display the games list on the page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import axios from 'axios'
+// import axios from 'axios'
 import { getGamesAmerica } from 'nintendo-switch-eshop'
-// axios.get('http://www.nintendo.com/json/content/get/filter/game?system=switch&sort=title&direction=asc&shop=ncom')
-// .then(res => {
-//   console.log(res)
-// })
+  // axios.get('http://www.nintendo.com/json/content/get/filter/game?system=switch&sort=title&direction=asc&shop=ncom')
+  // .then(res => {
+  //   console.log(res)
+  // })
 
-getGamesAmerica()
-.then( res => {
-  console.log(res)
-})
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      games: []
+    }
+  }
+
+
+  componentWillMount() {
+    getGamesAmerica()
+    .then( games => {
+      console.log(games)
+      this.setState({
+        games
+      })
+      console.log(this.state)
+    })
+  }
+
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>test</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.games}
         </p>
       </div>
     );
